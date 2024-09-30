7735d543a783f6d9029b2cc5ef74e632ec39584a
ritesh1712 <105135021+ritesh1712@users.noreply.github.com>
2024-09-30 14:26:30 +0530

    complete assessment

--- program1.js
+++ program1.js
@@ -3,9 +3,30 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+
+    for (const char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+           
+            stack.push(char);
+        } else if (char === ')' || char === '}' || char === ']') {
+            
+            if (stack.length === 0) {
+                return false; 
+            }
+            const last = stack.pop(); //top element
+            if (
+                (char === ')' && last !== '(') ||
+                (char === '}' && last !== '{') ||
+                (char === ']' && last !== '[')
+            ) {
+                return false; // Mismatch found
+            }
+        }
+    }
+
+    return stack.length === 0; // Valid if stack is empty
 };
 
+
 module.exports = { isValid };
-
-
